b87bf8ca4e9ee31b242b779221faecfe59c5e6f2
Yoe.com <liad.7735@gmail.com>
2025-08-14 20:45:18 +0300

    add delete line button and css update

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -2,6 +2,7 @@
 let gElCanvas
 let gCtx
 var currMemeId
+let gRect = {}
 
 
 
@@ -61,7 +62,7 @@ function getGctx() {
 function drawAllTextLines() {
     const meme = loadFromStorage(currMemeId)
     meme.lines.forEach((line, idx) => {
-        gCtx.font = `bold ${line.size}px Arial`
+        gCtx.font = `bold ${line.size}px meme`
         gCtx.fillStyle = line.color
         gCtx.strokeStyle = 'black'
         gCtx.lineWidth = 2
@@ -69,14 +70,26 @@ function drawAllTextLines() {
 
         gCtx.fillText(line.txt, line.x, line.y)
         gCtx.strokeText(line.txt, line.x, line.y)
+        meme.lines[meme.selectedLineIdx].txtSize = gCtx.measureText(meme.lines[meme.selectedLineIdx].txt).width
+        const size = meme.lines[meme.selectedLineIdx].txtSize
+        console.log(size)
+        // createRect(line.x, line.y, size, 'red')
     })
+}
+
+function createRect(x = 0, y = 0, size = 0, color = 'transparent') {
+    gCtx.strokeStyle = color
+    gCtx.lineWidth = 1
+    gRect = { x: x / 1.8, y: y - 30, size1: size + 5, size2: size / 4.5, }
+    gCtx.strokeRect(gRect.x, gRect.y, gRect.size1, gRect.size2)
 }
 
 function onSwitchLine() {
     const meme = getGmeme()
     meme.selectedLineIdx = meme.selectedLineIdx === 0 ? 1 : 0
     const elInput = getTxtInput()
-    elInput.value = gMeme.lines[gMeme.selectedLineIdx].txt
+    console.log(meme.lines[meme.selectedLineIdx].txt)
+    elInput.value = meme.lines[meme.selectedLineIdx].txt
     elInput.placeholder = ''
     renderMeme()
 }
@@ -94,13 +107,22 @@ function onImgSelect(imgId) {
 }
 
 function downloadImg(elLink) {
-    const img = findImg(currMemeId)
     const dataUrl = gElCanvas.toDataURL()
+    const { x, y, siz1, size2 } = gRect
+    gCtx.clearRect(x, y, siz1, size2)
 
     elLink.href = dataUrl
     elLink.download = 'my-image.jpg'
 }
 
+function onDeleteLine() {
+    const meme = getGmeme()
+     meme.lines.splice(meme.selectedLineIdx,1)
+     meme.selectedLineIdx = meme.selectedLineIdx === 0 ? 1 : 0
+     const elInput = getTxtInput()
+     elInput.value = meme.lines[meme.selectedLineIdx].txt
+     renderMeme()
+}
 
 function onSetLineColor(val) {
     SetLineColor(val)
@@ -123,7 +145,7 @@ function onIncreaseFont() {
 
 function onShowLineData(ev) {
     const { offsetX, offsetY } = ev
-    const meme = getGmeme()
+    const meme = loadFromStorage(currMemeId)
 
     const clickedLine = meme.lines.find(line => {
         return (
@@ -146,3 +168,30 @@ function onAddLine() {
     renderMeme()
 }
 
+function onUploadToFB(url) {
+    // console.log('url:', url)
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}&t=${url}`)
+}
+
+
+function onUploadImg(ev) {
+    ev.preventDefault()
+    const canvasData = gElCanvas.toDataURL('image/jpeg')
+
+    function onSuccess(uploadedImgUrl) {
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.btn-download-fb').innerHTML = `
+            <a href="${uploadedImgUrl}">Image Url</a>
+            <p>Image url: ${uploadedImgUrl}</p>
+           
+            <button class="btn-facebook" target="_blank" onclick="onUploadToFB('${encodedUploadedImgUrl}')">
+                Share on Facebook  
+            </button>
+             <a href="#" target="_blank" onclick="onUploadToFB('${encodedUploadedImgUrl}')">
+                     Facebook
+                    </a>
+        `
+    }
+
+    uploadImg(canvasData, onSuccess)
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -9,27 +9,16 @@ var gMeme = {
         {
             txt: 'I sometimes eat Falafel'
             ,
-            size: 20,
-            color: 'red',
+            size: 40,
+            color: 'white',
             x: elCanvas.width / 2,
             y: elCanvas.height / 2,
+            txtSize: 0,
         }
     ]
 }
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 var storageMemes = `${gMeme.selectedImgId}`
-
-
-function _makeGimgs() {
-
-    for (let i = 2; i <= 18; i++) {
-        gImgs.push({
-            id: i,
-            url: `img/${i}.jpg`,
-            keywords: []
-        })
-    }
-}
 
 
 
@@ -38,9 +27,29 @@ function getGmeme() {
 }
 
 function getgImg() {
-    _makeGimgs()
-    return gImgs
+  
+  return  [
+        { id: 1, url: 'img/1.jpg', keywords: ['trump', 'politican'] },
+        { id: 2, url: 'img/2.jpg', keywords: ['dog', 'love'] },
+        { id: 3, url: 'img/3.jpg', keywords: ['dog', 'sleep'] },
+        { id: 4, url: 'img/4.jpg', keywords: ['cat', 'sleep'] },
+        { id: 5, url: 'img/5.jpg', keywords: ['baby', 'funny'] },
+        { id: 6, url: 'img/6.jpg', keywords: ['man', 'hair'] },
+        { id: 7, url: 'img/7.jpg', keywords: ['baby', 'funny'] },
+        { id: 8, url: 'img/8.jpg', keywords: ['wonka', 'chocolate'] },
+        { id: 9, url: 'img/9.jpg', keywords: ['baby', 'laughing'] },
+        { id: 10, url: 'img/10.jpg', keywords: ['obama','politican'] },
+        { id: 11, url: 'img/11.jpg', keywords: ['sport', 'kiss'] },
+        { id: 12, url: 'img/12.jpg', keywords: ['point', 'glasses'] },
+        { id: 13, url: 'img/13.jpg', keywords: ['leo', 'toast'] },
+        { id: 14, url: 'img/14.jpg', keywords: ['matrix', 'glasses'] },
+        { id: 15, url: 'img/15.jpg', keywords: ['lord', 'rings'] },
+        { id: 16, url: 'img/16.jpg', keywords: ['galaxy', 'laughing'] },
+        { id: 17, url: 'img/17.jpg', keywords: ['putin', 'politicans'] },
+        { id: 18, url: 'img/18.jpg', keywords: ['toy', 'story'] }
+    ]
 }
+
 
 function SetLineColor(color) {
     gMeme.lines[gMeme.selectedLineIdx].color = color
